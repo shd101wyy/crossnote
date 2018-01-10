086a985cec6f5d1b777bd6d7370a929efd9f71e3
Alexander Kachkaev <alexander@kachkaev.ru>
2018-01-10 15:44:16 +0300

    Refactor tests for code-chunk-attributes.ts to support more than one raw value per case

--- src/__tests__/code-chunk-attributes.ts
+++ src/__tests__/code-chunk-attributes.ts
@@ -2,58 +2,61 @@ import { parseAttributes } from '../code-chunk-attributes';
 
 const testCases = [{
   // classic behavior
-  text: 'cmd=true',
-  expectedResult: { cmd: true }
+  raw: ['cmd=true', '{cmd=true}', '  {  cmd=true  }  '],
+  expectedAttributes: { cmd: true }
 }, {
-  text: 'cmd=true hello=world',
-  expectedResult: { cmd: true, hello: 'world' }
+  raw: 'cmd=true hello=world',
+  expectedAttributes: { cmd: true, hello: 'world' }
 }, {
-  text: 'cmd=true hello=true',
-  expectedResult: { cmd: true, hello: true }
+  raw: 'cmd=true hello=true',
+  expectedAttributes: { cmd: true, hello: true }
 }, {
-  text: 'cmd=true hello="true"',
-  expectedResult: { cmd: true, hello: 'true' }
+  raw: 'cmd=true hello="true"',
+  expectedAttributes: { cmd: true, hello: 'true' }
 }, {
-  text: 'cmd=true hello=\'true\'',
-  expectedResult: { cmd: true, hello: 'true' }
+  raw: 'cmd=true hello=\'true\'',
+  expectedAttributes: { cmd: true, hello: 'true' }
 }, {
-  text: 'cmd=true .class1',
-  expectedResult: { cmd: true, class: 'class1' }
+  raw: 'cmd=true .class1',
+  expectedAttributes: { cmd: true, class: 'class1' }
 }, {
-  text: 'cmd=true #some-id',
-  expectedResult: { cmd: true, id: 'some-id' }
+  raw: 'cmd=true #some-id',
+  expectedAttributes: { cmd: true, id: 'some-id' }
 }, {
-  text: 'cmd=true .class1 .class2',
-  expectedResult: { cmd: true, class: 'class1 class2' }
+  raw: 'cmd=true .class1 .class2',
+  expectedAttributes: { cmd: true, class: 'class1 class2' }
 }, {
-  text: '.class1 cmd=true .class2',
-  expectedResult: { cmd: true, class: 'class1 class2' }
+  raw: '.class1 cmd=true .class2',
+  expectedAttributes: { cmd: true, class: 'class1 class2' }
 }, {
-  text: 'cmd=true args=["-v"]',
-  expectedResult: { cmd: true, args: ['-v'] }
+  raw: 'cmd=true args=["-v"]',
+  expectedAttributes: { cmd: true, args: ['-v'] }
 }, {
-  text: 'cmd=true args=["-i", "$input_file", "-o", "./output.png"] .class1',
-  expectedResult: { cmd: true, args: ['-i', '$input_file', '-o', './output.png'], class: 'class1' }
+  raw: 'cmd=true args=["-i", "$input_file", "-o", "./output.png"] .class1',
+  expectedAttributes: { cmd: true, args: ['-i', '$input_file', '-o', './output.png'], class: 'class1' }
 }, {
   // shortcuts
-  text: 'cmd hide',
-  expectedResult: { cmd: true, hide: true }
+  raw: 'cmd hide',
+  expectedAttributes: { cmd: true, hide: true }
 }, {
-  text: 'cmd output="path.html" hide',
-  expectedResult: { cmd: true, output: 'path.html', hide: true }
+  raw: 'cmd output="path.html" hide',
+  expectedAttributes: { cmd: true, output: 'path.html', hide: true }
 }, {
-  text: 'cmd hide .class1',
-  expectedResult: { cmd: true, hide: true, class: 'class1' }
+  raw: 'cmd hide .class1',
+  expectedAttributes: { cmd: true, hide: true, class: 'class1' }
 }, {
-  text: 'cmd .class1 hide .class2',
-  expectedResult: { cmd: true, hide: true, class: 'class1 class2' }
+  raw: 'cmd .class1 hide .class2',
+  expectedAttributes: { cmd: true, hide: true, class: 'class1 class2' }
 }]
 
 describe('code-chunk-attributes', () => {
-  testCases.map(({ text, expectedResult }) => {
-    it(`parseAttributes() correctly parses ${text}`, () => {
-      const result = parseAttributes(text)
-      expect(result).toEqual(expectedResult);
-    });
+  testCases.map(({ raw, expectedAttributes }) => {
+    const arrayOfTexts = typeof raw === 'string' ? [raw] : raw
+    arrayOfTexts.map(text => {
+      it(`parseAttributes() correctly parses ${text}`, () => {
+        const result = parseAttributes(text)
+        expect(result).toEqual(expectedAttributes);
+      });
+    })
   });
 })
