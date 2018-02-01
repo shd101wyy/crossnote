9ee5f814b8d1358c5333eb9c71157b7f33daf41e
Alexander Kachkaev <alexander@kachkaev.ru>
2018-02-01 21:38:09 +0000

    Move jest config out of package.json and configure coverage reports to list all src files

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mapCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx,js}",
+  ],
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testMatch: ["**/__tests__/*.(ts|tsx|js)"],
+  testPathIgnorePatterns: ["/node_modules/", "/out/"]
+};
