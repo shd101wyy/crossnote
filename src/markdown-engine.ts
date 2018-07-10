56eba6a5a95225d5e46dc2445650f2ef4cf29b4d
Alter <951912681@qq.com>
2018-07-10 11:06:14 +0800

    语法优化: 使用const替换let, 以增强代码约束.

--- src/markdown-engine.ts
+++ src/markdown-engine.ts
@@ -1378,12 +1378,12 @@ sidebarTOCBtn.addEventListener('click', function(event) {
 })
 
 function scrollSmooth(htmlElement) {
-  let anchor = document.getElementById(htmlElement.getAttribute("href").substring(1));
+  const anchor = document.getElementById(htmlElement.getAttribute("href").substring(1));
   window.scrollTo({ "behavior": "smooth", "top": anchor.offsetTop });
 }
 
 function setSelectionStyle(htmlElement) {
-  htmlElement.setAttribute("style", "border-left: 4px solid #2089C4; padding-left: 5px")
+  htmlElement.setAttribute("style", "border-left: 4px solid #2089C4; padding-left: 5px;")
 }
 
 function rmSelectionStyle() {
