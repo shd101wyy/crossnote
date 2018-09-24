24bbaedb52d37ef4d41f1c873eff8a2a2bb795d2
Yiyi Wang <shd101wyy@gmail.com>
2018-09-24 16:02:15 -0500

    Lazily load viz.js (#92)

--- src/viz.ts
+++ src/viz.ts
@@ -13,17 +13,20 @@ const { Module, render } = require(resolve(
   "./dependencies/viz/full.render.js",
 ));
 
-let viz = new _VIZ({ Module, render });
+let viz = null;
 /**
  *
  * @param renderOption https://github.com/mdaines/viz.js/wiki/API#render-options
  */
 export async function Viz(digraph: string, renderOption: object) {
   try {
+    if (!viz) {
+      viz = new _VIZ({ Module, render });
+    }
     return await viz.renderString(digraph, renderOption);
   } catch (error) {
     // Create a new Viz instance (@see Caveats page for more info)
-    viz = new _VIZ({ Module, render });
+    viz = null;
     throw error;
   }
 }
