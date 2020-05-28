0509f46afbb43bb55a346f91e5816408de71ae11
JardinHotel <64269405+JardinHotel@users.noreply.github.com>
2020-05-29 00:09:46 +0900

    delete code block diagram options from gfm

--- src/markdown-convert.ts
+++ src/markdown-convert.ts
@@ -314,6 +314,7 @@ export async function markdownConvert(
           graphsCache,
           imageMagickPath,
           mermaidTheme,
+          addOptionsStr: false,
         }).then(({ outputString }) => {
           outputString = data.frontMatterString + outputString; // put the front-matter back.
 
