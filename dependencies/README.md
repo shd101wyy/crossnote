d787d7b37e0138bef3619addf7cab8871acac7b6
shd101wyy <shd101wyy@gmail.com>
2023-03-05 16:54:30 +0800

    feat: Updated mermaid to 9.4.0

--- dependencies/README.md
+++ dependencies/README.md
@@ -4,7 +4,7 @@ I managed many libraries by myself instead of through npm to reduce overall file
 
 ```json
 {
-  "mermaid": "9.1.7",
+  "mermaid": "9.4.0",
   "plantuml": "1.2023.2",
   "wavedrom": "2.9.1",
   "reveal": "4.10.0",
--- src/markdown-engine.ts
+++ src/markdown-engine.ts
@@ -569,7 +569,7 @@ if (typeof(window['Reveal']) !== 'undefined') {
   Reveal.addEventListener('ready', mermaidRevealHelper)
 } else {
   // The line below will cause mermaid bug in preview.
-  // mermaid.init(null, document.getElementsByClassName('mermaid'))
+  // mermaid.init(null, document.querySelectorAll('.mermaid'))
 }
 </script>`;
 
@@ -1139,7 +1139,7 @@ if (typeof(window['Reveal']) !== 'undefined') {
           "./dependencies/mermaid/mermaid.min.js",
         )}" charset="UTF-8"></script>`;
       } else {
-        mermaidScript = `<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/mermaid@9.1.7/dist/mermaid.min.js"></script>`;
+        mermaidScript = `<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/mermaid@9.4.0/dist/mermaid.min.js"></script>`;
       }
       const mermaidConfig: string = await utility.getMermaidConfig(
         this.config.configPath,
@@ -1168,7 +1168,7 @@ if (typeof(window['Reveal']) !== 'undefined') {
   Reveal.addEventListener('slidechanged', mermaidRevealHelper)
   Reveal.addEventListener('ready', mermaidRevealHelper)
 } else {
-  mermaid.init(null, document.getElementsByClassName('mermaid'))
+  mermaid.init(null, document.querySelectorAll('mermaid'))
 }
 </script>`;
     }
