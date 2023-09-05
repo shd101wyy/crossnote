b944b1d87e4d8f76707769e542c8ceb89a9f1725
shd101wyy <shd101wyy@gmail.com>
2023-09-05 09:53:27 +0800

    fix: Fallback to GET for the official puml server

--- src/renderers/puml-server.ts
+++ src/renderers/puml-server.ts
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch';
+import plantumlEncoder from 'plantuml-encoder';
 import { Readable } from 'stream';
 
 export default class PlantUMLServerTask {
@@ -9,14 +10,23 @@ export default class PlantUMLServerTask {
   }
 
   public generateSVG(content: string): Promise<string> {
-    const contentStream = new Readable();
-    contentStream.setEncoding('utf-8');
-    contentStream.push(content);
-    contentStream.push(null); // Mark end of stream
-    return fetch(this.serverURL, {
-      method: 'POST',
-      body: contentStream,
-      headers: { 'Content-Type': 'text/plain; charset=utf-8' },
-    }).then(res => res.text());
+    if (this.serverURL.match(/^https?:\/\/(www\.)?plantuml\.com\/plantuml/)) {
+      // NOTE: The official plantuml server doesn't support POST method,
+      // so we fallback to encode the content and send it as a GET request.
+      const encoded = plantumlEncoder.encode(content);
+      return fetch(
+        `http://www.plantuml.com/plantuml/svg/${encoded}`,
+      ).then(res => res.text());
+    } else {
+      const contentStream = new Readable();
+      contentStream.setEncoding('utf-8');
+      contentStream.push(content);
+      contentStream.push(null); // Mark end of stream
+      return fetch(this.serverURL, {
+        method: 'POST',
+        body: contentStream,
+        headers: { 'Content-Type': 'text/plain; charset=utf-8' },
+      }).then(res => res.text());
+    }
   }
 }
