d629fd6b65a2fa136615a0519f0ae88122cb1ed1
shd101wyy <shd101wyy@gmail.com>
2023-09-13 16:41:14 +0800

    fix: Fixed the ci
    ci: Removed some husky git hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,4 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
-  ignorePatterns: [
-    'build.js',
-    'gulpfile.js',
-    'tailwind.config.js',
-    '.eslintrc.js',
-    'jest.config.js',
-    'prettier.config.js',
-    'test.js',
-    'node_modules/**',
-    'out/**',
-    'styles/**',
-    'dependencies/**',
-    'src/prism/prism.js',
-  ],
 };
