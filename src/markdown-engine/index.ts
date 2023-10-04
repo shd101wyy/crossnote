aba8141dab196e8fc1d302bb8624166e104ced85
shd101wyy <shd101wyy@gmail.com>
2023-10-04 16:10:10 +0800

    feat: Updated mermaid to 10.5.0

--- src/markdown-engine/index.ts
+++ src/markdown-engine/index.ts
@@ -793,7 +793,7 @@ window["initRevealPresentation"] = async function() {
           './dependencies/mermaid/mermaid.min.js',
         )}" charset="UTF-8"></script>`;
       } else {
-        mermaidScript = `<script src="https://${this.notebook.config.jsdelivrCdnHost}/npm/mermaid@10.4.0/dist/mermaid.min.js"></script>`;
+        mermaidScript = `<script src="https://${this.notebook.config.jsdelivrCdnHost}/npm/mermaid@10.5.0/dist/mermaid.min.js"></script>`;
       }
 
       mermaidInitScript += `<script type="module">
